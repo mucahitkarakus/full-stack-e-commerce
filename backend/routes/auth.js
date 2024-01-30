cc4816c43840b3fd03f7d385f320d36e79084a9e
mucahitkarakus <mocococo.80@gmail.com>
2024-01-30 16:24:36 +0300

    Add unique constraint to email field and hash user password

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+
+
 
 // Register
 router.post('/register', async (req, res) => {
     try {
         const { username, email, password } = req.body
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) return res.status(400).json({ err: 'User already exists' });
+
         const newUser = await new User({
             username,
             email,
-            password
+            password: hashedPassword
         });
 
         await newUser.save();
