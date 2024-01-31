5c74fdcbf33ad799eff9a9727d06d52975756044
mucahitkarakus <mocococo.80@gmail.com>
2024-01-31 14:54:40 +0300

    Add Coupon model and update Coupons route

--- backend/routes/coupons.js
+++ backend/routes/coupons.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Coupon = require('../models/Coupons');
+const Coupon = require('../models/Coupon');
 
 // Yeni Kupon Ekleme (Create)
 router.post('/', async (req, res) => {
@@ -10,6 +10,35 @@ router.post('/', async (req, res) => {
         res.status(201).json(newCoupon)
     } catch (error) {
         console.log(error)
+    }
+});
+
+
+// Tümü Kuponlar (Read)
+router.get('/', async (req, res) => {
+    try {
+        const coupons = await Coupon.find()
+        res.status(201).json(coupons)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ error: 'Server Error' })
+    }
+});
+
+// Tek Kuponlar (Single)
+router.get('/:couponId', async (req, res) => {
+    try {
+        const couponId = req.params.couponId
+        try {
+            const coupon = await Coupon.findById(couponId)
+            res.status(200).json(coupon)
+        } catch (error) {
+            console.log(error)
+            res.status(404).json({ error: 'Category Not found' })
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ error: 'Server Error' })
     }
 });
 
