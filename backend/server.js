c4de28dcf970aa7a64c0a1667db3a63111abbdcc
mucahitkarakus <mocococo.80@gmail.com>
2024-01-31 16:11:32 +0300

    backend bind front end

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,9 @@ const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const mainRoute = require('./routes/index');
+const cors = require('cors'); // CORS modülünü ekleyin
 const port = 5000;
 const logger = require('morgan');
-
 
 dotenv.config();
 
@@ -18,13 +18,12 @@ const connect = async () => {
     }
 }
 
-//Middleware
+// Middleware
+app.use(cors()); // CORS middleware eklendi
 app.use(logger('dev'));
 app.use(express.json());
 
-
 app.use('/api', mainRoute)
-
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
